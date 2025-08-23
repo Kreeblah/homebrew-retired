984568fd81b24d7c17ac7d06023ba043ae502c11
Kreeblah <kreeblah@gmail.com>
2025-08-22 19:42:10 -0700

    Initial import

---
+++ Casks/schism-tracker.rb
@@ -0,0 +1,21 @@
+cask "schism-tracker" do
+  version "20250728"
+  sha256 "da183ba39ae38fe1fb4b2bdcb036b3487beb77bb67d0c54e9c9b437a00449a0f"
+
+  url "https://github.com/schismtracker/schismtracker/releases/download/#{version}/schismtracker-#{version}-macos.zip"
+  name "Schism Tracker"
+  desc "Oldschool sample-based music composition tool"
+  homepage "https://github.com/schismtracker/schismtracker"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)*)$/i)
+  end
+
+  app "Schism Tracker.app"
+
+  zap trash: [
+    "~/Library/Application Support/Schism Tracker",
+    "~/Library/Saved Application State/org.schismtracker.SchismTracker.savedState",
+  ]
+end
---
+++ Casks/xemu.rb
@@ -0,0 +1,23 @@
+cask "xemu" do
+  version "0.8.96"
+  sha256 "5269affa4e9b9cf97b5d1da6dfd22820c5b228cc81aec9935d6bb598c865bd4e"
+
+  url "https://github.com/xemu-project/xemu/releases/download/v#{version}/xemu-macos-universal-release.zip",
+      verified: "github.com/xemu-project/xemu/"
+  name "Xemu"
+  desc "Original Xbox Emulator"
+  homepage "https://xemu.app/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Xemu.app"
+
+  zap trash: [
+    "~/Library/Application Support/xemu",
+    "~/Library/Preferences/xemu.app.0.plist",
+    "~/Library/Saved Application State/xemu.app.0.savedState",
+  ]
+end
